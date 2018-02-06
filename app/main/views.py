0da4fab5a6757b607135ef2587a516161f523392
munenelewis <munene2379@gmail.com>
2018-02-06 16:48:57 +0300

    made a connection to the db and displayed the comments

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from . import main
 from flask import render_template,request
 from flask_login import login_required
 from flask_login import login_required
+from ..models import Category
 
 
 @main.route('/')
@@ -11,58 +12,5 @@ def index():
     '''
     title = 'Home - Welcome to The best Pitching Website Online'
 
-    return render_template('index.html',title=title)
-# @main.route('/login')
-# def login():
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     title = 'Home - Welcome to The best Pitching Website Online'
-
-#     return render_template('login.html',title=title)
-
-@main.route('/interview')
-def interview():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The best Pitching Website Online'
-
-    return render_template('interview.html',title=title)
-
-@main.route('/pickup')
-def pickup():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The best Pitching Website Online'
-
-    return render_template('pickup.html')
-
-@main.route('/promo')
-def promo():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The best Pitching Website Online'
-
-    return render_template('promotionpitch.html')
-
-@main.route('/product')
-def product():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The best Pitching Website Online'
-
-    return render_template('product.html')
-
-@main.route('/coment')
-@login_required
-def coment():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The best Pitching Website Online'
-
-    return render_template('comments.html')
+    categories = Category.get_categories()
+    return render_template('index.html',title=title,categories=categories)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,4 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
-from . import login_manager
 
 
 
@@ -9,56 +6,21 @@ class Pitch:
     def __init__(self,pitch):
         self.pitch = pitch
     
-class Category:
-     __tablename__='users'
-     pickup = db.Column(db.String(255))
-
-
-class User(UserMixin,db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    email = db.Column(db.String(255),unique = True,index = True)
-    password_hash = db.Column(db.String(255))
+    
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
-
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     def __repr__(self):
         return f'User {self.username}'
-
-class Submission(db.Model):
-    __tablename__ = 'Submission'
-    id = db.Column(db.Integer,primary_key = True)
-    details = db.Column(db.String(255))
-
-class Profile(db.Model):
-    __tablename__ = 'Profile'
-    id = db.Column(db.Integer,primary_key = True)
-    details = db.Column(db.String(255))
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    nickname = db.Column(db.String(255))
-class Role(db.Model):
-    __tablename__ = 'roles'
-
+class Category(db.Model):
+    __tablename_='categories'
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-    def __repr__(self):
-        return f'User {self.name}'
+    @classmethod
+    def get_categories(cls):
+        categories = Category.query.all()
+
+        return categories
