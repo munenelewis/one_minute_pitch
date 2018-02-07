9e3686f44f093a3ef74c354dbececf72d5b528f2
munenelewis <munene2379@gmail.com>
2018-02-07 09:55:22 +0300

    added emails configurations

--- config.py
+++ config.py
@@ -5,6 +5,16 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://anynonimus:anynonimus@localhost/pitch'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    @staticmethod
+    def init_app(app):
+        app
 
 class ProdConfig(Config):
     '''
