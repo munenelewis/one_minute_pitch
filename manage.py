c70116169b47df66956be4723bd6bed984f8b5c6
munenelewis <munene2379@gmail.com>
2018-02-09 01:40:50 +0300

    created a new form called comments and passed it into the mNge.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User,Pitch
+from app.models import User,Pitch,Comment
 from  flask_migrate import Migrate, MigrateCommand
 
 
@@ -17,7 +17,7 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Pitch= Pitch)
+    return dict(app = app,db = db,User = User, Pitch= Pitch,Comment=Comment)
 
 if __name__ == '__main__':
     manager.run()
